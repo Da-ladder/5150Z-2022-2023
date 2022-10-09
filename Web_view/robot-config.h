2a2992875322b70d5628adc19a06f35c9bc0c021
Da-ladder <115005260+Da-ladder@users.noreply.github.com>
2022-10-09 15:39:23 -0400

    Update robot-config.h

--- Web_view/robot-config.h
+++ Web_view/robot-config.h
@@ -3,8 +3,20 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
-extern drivetrain Drivetrain;
+extern drivetrain chassis;
 extern controller Controller1;
+
+extern motor_group leftDrive;
+extern motor_group rightDrive;
+
+extern motor leftMotorA;
+extern motor leftMotorB;
+extern motor rightMotorA;
+extern motor rightMotorB;
+
+extern void chassisControlTank();
+extern void chassisControlArcade();
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
